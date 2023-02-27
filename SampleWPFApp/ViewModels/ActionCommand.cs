259a1ceca1a1622e8b90498b73f0f0062c9c846a
hmadrigal <hfmad@hotmail.com>
2023-02-27 11:57:12 -0600

    Add project files.

---
+++ SampleWPFApp/ViewModels/ActionCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SampleWPFApp.ViewModels
+{
+    public class ActionCommand<T> : ICommand
+    {
+        public Action<T> Action { get; set; } = _ => { };
+        public Func<T, bool> Predicate { get; set; } = _ => true;
+
+        public ActionCommand(Action<T> action)
+        {
+            Action = action;
+        }
+
+        public ActionCommand(Action<T> action, Func<T, bool> predicate)
+        {
+            Action = action;
+            Predicate = predicate;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter is T p)
+            { return Predicate.Invoke(p); }
+
+            return false;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (CanExecute(parameter) && parameter is T p)
+            { Action?.Invoke(p); }
+        }
+    }
+
+}
---
+++ SampleWPFApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleWPFApp.ViewModels
+{
+
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _item = string.Empty;
+        public string Item
+        {
+            get { return _item; }
+            set { SetField(ref _item, value, nameof(Item)); }
+        }
+
+        public ICommand AddCommand { get; }
+
+        public ICommand RemoveCommand { get; }
+
+        public ObservableCollection<string> Items { get; private set; } = new ObservableCollection<string>();
+        public MainWindowViewModel()
+        {
+            AddCommand = new ActionCommand<string>(ExecuteAddCommand, CanExecuteAddCommand);
+            RemoveCommand = new ActionCommand<string>(ExecuteRemoveCommand, CanExecuteRemoveCommand);
+        }
+
+        private void ExecuteRemoveCommand(string arg)
+        {
+            Items.Remove(arg);
+        }
+
+        private bool CanExecuteRemoveCommand(string arg) => !string.IsNullOrEmpty(arg);
+
+        private bool CanExecuteAddCommand(string arg) => !string.IsNullOrWhiteSpace(arg);
+
+        private void ExecuteAddCommand(string text)
+        {
+            Items.Add(text);
+            Item = string.Empty;
+        }
+    }
+
+}
